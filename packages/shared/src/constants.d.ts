9bd92a048c79a03440cbd6cad55371a346f71074
fayyozbobokulov <fayyoz.bobokulov@praoshealth.com>
2025-02-16 18:31:40 -0800

    fix: removed the wrongly build packages

--- packages/shared/src/constants.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const API_ENDPOINTS: {
-    readonly ANTIQUES: "/api/antiques";
-    readonly UPLOAD: "/api/upload";
-};
-//# sourceMappingURL=constants.d.ts.map
--- packages/shared/src/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_ENDPOINTS = void 0;
-// Define shared constants here
-exports.API_ENDPOINTS = {
-    ANTIQUES: '/api/antiques',
-    UPLOAD: '/api/upload',
-};
-//# sourceMappingURL=constants.js.map
